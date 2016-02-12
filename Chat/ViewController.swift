db7c32974a784dd91ec51f484ec26bcc45410fb6
eMobc SL <emobc@eMobcs-MacBook-Pro.local>
2016-02-11 19:43:16 -0800

    First chat implementation

---
+++ Chat/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by eMobc SL on 11/02/16.
+//  Copyright © 2016 Neurowork. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,5 @@
+use_frameworks!
+
+target 'Chat' do
+  pod 'Parse'
+end
